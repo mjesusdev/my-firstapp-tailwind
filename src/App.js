8bdad669f55096166a261b7c43e4b615af63e1bd
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-03-10 12:09:46 +0100

    Continue learning Tailwind, add more style button & open an alert :rocket:

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const handleClick = () => {
+  alert('My First Alert in Tailwind 🚀🚀');
+  // px - left & right padding
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-5xl text-sky-400/100 font-bold underline">
+        <img src={logo} className="App-logo" alt="React Icon 🚀" />
+        <h1 className="text-5xl text-sky-400/100 font-bold mb-5">
           Hello world!
         </h1>
+        <button onClick={handleClick}
+          className="bg-sky-500 opacity-100 font-bold tracking-tighter rounded padding shadow-lg shadow-sky-500/50 hover:bg-sky-700 p-2">
+            Open an Alert 🙈
+        </button>
       </header>
+      {/* // TODO: Create components & Footer to add the repository & more */}
     </div>
   );
 }
