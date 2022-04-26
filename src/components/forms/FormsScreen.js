ed3f354a238bb5b8d2f70bf963fd823fbbb50b34
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-04-26 12:17:37 +0200

    Implement form to FormsScreen :rocket:

--- src/components/forms/FormsScreen.js
+++ src/components/forms/FormsScreen.js
@@ -13,8 +13,34 @@ export const FormsScreen = () => {
         <img src={logo} className="App-logo 2xl:object-scale-down" alt="React Icon 🚀" />
         <div className='text-5xl font-extrabold mb-5'>
             <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
-                Forms!
+                Form
             </span>
+            <div class="grid grid-cols-1 gap-6 mt-5">
+              <label class="block text-left">
+                <span class="text-white-700 text-sm">Username</span>
+                <input type="text" class="
+                    mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-gray-300
+                    shadow-sm
+                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                  " placeholder="Username" data-ddg-inputtype="identities.fullName" />
+              </label>
+              <label class="block text-left">
+                <span class="text-white-700 text-sm">Email address</span>
+                <input type="email" class="
+                    mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-gray-300
+                    shadow-sm
+                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                  " placeholder="user@user.com" data-ddg-inputtype="identities.emailAddress" data-ddg-autofill="true" />
+              </label>
+            </div>
         </div>
     </header>
     // TODO: Create components & Footer to add the repository & more
